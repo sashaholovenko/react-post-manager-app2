227fce9994157a4571f29ae580d0a8ecb9e9ec3e
sashaholovenko <sasha155s49@gmail.com>
2022-09-22 02:40:55 +0300

    app published
    app published

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            This application was created by Sasha Holovenko. The purpose of app is to be part of porfolio and show skills in main React technologies and patterns.
+        </h1>
+    );
+};
+
+export default About;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1 style={{color: 'red'}}>
+                You have opened a non-existent page, please try again
+            </h1>
+        </div>
+    );
+};
+
+export default Error;
